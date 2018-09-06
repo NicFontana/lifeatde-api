8cce078fc8dd15528acdeaa84bf92984c09f56cd
Niccolò Fontana <niccolo.fontana@student.unife.it>
2018-09-06 18:46:41 +0200

    Corrette le routes e aggiornato il gitignore

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  resources :documents
-  resources :projects
-  resources :categories
-  resources :study_groups
-  resources :courses do
-    resources :news, only: [:show]
-    get 'news', :to => 'news#course_news'
-  end
-  resources :users
-  post 'login', :to => 'authentication#login'
+	scope :api do
+		resources :documents
+		resources :projects
+		resources :categories
+		resources :study_groups
+		resources :courses do
+			resources :news, only: [:show]
+			get 'news', :to => 'news#course_news'
+		end
+		resources :users
+		post 'login', :to => 'authentication#login'
+	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
