23de74e352743658fd36d2d2786e433fe885008c
Federico Frigo <ffrigo8@gmail.com>
2018-09-08 17:24:03 +0200

    Gestione membri dei progetti

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,46 @@
 class UsersController < ApplicationController
   include Pagination
+  include Rack::Utils
   before_action :set_user, only: [:show, :update, :destroy]
 
-  # GET /users/1
+  # GET /users/:id
   def show
     render json: UserSerializer.new(@user).serialized_json
   end
 
-  # PATCH/PUT /users/1
+  # PATCH/PUT /users/:id
   def update
     unless @user.id == auth_user.id
-      return render json: ErrorSerializer.new('You can update only your profile', Rack::Utils.status_code(:forbidden)).serialized_json, status: :forbidden
+      return render json: ErrorSerializer.new('You can update only your profile', status_code(:forbidden)).serialized_json, status: :forbidden
     end
 
     if @user.update(user_params)
       render json: UserSerializer.new(@user).serialized_json
     else
-			render json: ErrorSerializer.new(@user.errors, Rack::Utils.status_code(:unprocessable_entity)).serialized_json, status: :unprocessable_entity
+			render json: ErrorSerializer.new(@user.errors, status_code(:unprocessable_entity)).serialized_json, status: :unprocessable_entity
     end
   end
 
-  # DELETE /users/1
-  def destroy
-    @user.destroy
+  # POST /projects/:project_id/users/:id/add_member
+  def add
+    @member = @user.projects_users.new(project_id: params[:project_id], admin: false)
+
+    if @member.save
+      render json: UserSerializer.new(@user).serialized_json
+    else
+      render json: ErrorSerializer.new(@user.errors, status_code(:unprocessable_entity)).serialized_json, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /projects/:project_id/users/:id/add_member
+  def remove
+    @member = @user.projects_users.where(project_id: params[:project_id], admin: false)
+
+    if @member.destroy_all
+      render json: UserSerializer.new(@user).serialized_json
+    else
+      render json: ErrorSerializer.new(@user.errors, status_code(:unprocessable_entity)).serialized_json, status: :unprocessable_entity
+    end
   end
 
   private
