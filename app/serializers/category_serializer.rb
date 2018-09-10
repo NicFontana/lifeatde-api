dcd60b6cc513e13c219789c0da9039016e93d56e
Federico Frigo <ffrigo8@gmail.com>
2018-09-10 10:18:45 +0200

    fix

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,5 +1,5 @@
 class CategorySerializer
   include FastJsonapi::ObjectSerializer
   attributes :name
-  has_many :projects
+  has_many :projects, if: Proc.new { |record, params| record.association(:projects).loaded? }
 end
