31b363a52918cec1dfe0dd5b718d886e55d1fd0e
Federico Frigo <ffrigo8@gmail.com>
2018-10-01 16:32:20 +0200

    fix all'url di default

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Configure the default url options
+  Rails.application.routes.default_url_options[:host] = 'http://localhost:3000'
 end
