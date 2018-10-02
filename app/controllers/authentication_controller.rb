5f4bcfdf20a6a696ed1bdeb667efec9077905ee1
Niccolò Fontana <niccolo.fontana@student.unife.it>
2018-10-02 15:59:21 +0200

    Ora dopo il login sappiamo anche di che corso fa parte l'utente e quali sono le sue categorie preferite

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,11 +1,12 @@
 class AuthenticationController < ApplicationController
 	def login
-		@user = User.find_by(email: params[:email].to_s.downcase)
+		@user = User.includes(:course, :categories).find_by(email: params[:email].to_s.downcase)
 
 		if @user && @user.authenticate(params[:password])
 			token = JsonWebToken.encode({user_id: @user.id})
 
 			@serializer_options[:params] = {token: token}
+			@serializer_options[:include] = [:course, :categories]
 			@serializer_options[:meta][:messages] = ['Login effettuato con successo!']
 
 			render json: UserSerializer.new(@user, @serializer_options).serialized_json
